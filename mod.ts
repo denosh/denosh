e4f168f6b0923efeb1eabbe4ee881a80b5b45ee9
Richard Yu <vipzhicheng@gmail.com>
2020-07-29 14:26:56 +0800

    fix: adjust mod.ts

--- mod.ts
+++ mod.ts
@@ -1,3 +1,4 @@
 export * from './src/common/interface.ts'
 export { OptionManger } from './src/common/option.ts'
 export * as Utils from './src/common/utils.ts'
+export * from './src/common/command.ts'
